4387577ef5802729004ad9030ca42a5159089b7d
haikalalihakim <148456995+haikalalihakim@users.noreply.github.com>
2024-01-25 22:44:42 +0700

    Add files via upload

---
+++ Jawabanno7.java
@@ -0,0 +1,21 @@
+
+public class Jawabanno7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int i, j;
+		for (i = 0; i <= n ; i++) {
+		for (i = 0; i < n ; i++) {
+			for (j = n; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (j = 0; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+			}
+		}
+	}
+
+}
